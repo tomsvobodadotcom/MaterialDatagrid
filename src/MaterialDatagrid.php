831232e7bc08e1d20ccf843c1d24f4996d726a29
Jan Holas <blackfox00@gmail.com>
2018-03-22 13:45:08 +0100

    basic usability markup & styles

--- src/MaterialDatagrid.php
+++ src/MaterialDatagrid.php
@@ -7,5 +7,26 @@ namespace TS\MaterialDatagrid;
  *
  * @author Tom Svoboda
  */
-class MaterialDatagrid extends \Ublaboo\Datagrid\Datagrid {
+class MaterialDatagrid extends \Ublaboo\DataGrid\DataGrid {
+
+    public function __construct() {
+        parent::__construct();
+        $this->useHappyComponents(FALSE);
+        $this->setTemplateFile(__DIR__ . "/templates/datagrid.latte");
+    }
+
+    public function getPaginator() {
+        $paginator = parent::getPaginator();
+        if ($paginator) {
+            $paginator->setTemplateFile(__DIR__ . "/templates/paginator.latte");
+        }
+        return $paginator;
+    }
+
+    public function addColumnStatus($key, $name, $column = null) {
+        $columnStatus = parent::addColumnStatus($key, $name, $column);
+        $columnStatus->setTemplate(__DIR__ . "/templates/column_status.latte");
+        return $columnStatus;
+    }
+
 }
